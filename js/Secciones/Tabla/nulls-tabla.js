1b3993cf593419d5581b6bd2d28334c3144ff216
Daniel Leon <erickdal448@gmail.com>
2023-09-04 23:13:12 -0700

    Solucion nulls

--- js/Secciones/Tabla/nulls-tabla.js
+++ js/Secciones/Tabla/nulls-tabla.js
@@ -4,14 +4,15 @@ let divContenedor = document.querySelector(".page");
 // Obtener el menú personalizado
 let menu = document.querySelector(".context-menu");
 
+// Crear una variable para almacenar una referencia al elemento td en el que se hizo clic derecho
+let tdSeleccionado;
+
 divContenedor.addEventListener("mousedown", function(e) {
   // Buscar el elemento tablaPadre en el árbol DOM ascendente desde el elemento en el que se hizo clic
-  let testPadre = e.target.closest(".tabla__tabla");
+  let tablaPadre = e.target.closest(".tabla__tabla");
   
   // Verificar si se encontró el elemento tablaPadre
-  if (testPadre) {
-    tablaPadre = testPadre;  
-
+  if (tablaPadre) {
     // Obtener todos los elementos td dentro del elemento tablaPadre
     let tds = tablaPadre.querySelectorAll("td");
 
@@ -21,26 +22,30 @@ divContenedor.addEventListener("mousedown", function(e) {
         // Prevenir la acción predeterminada del navegador al hacer clic derecho
         e.preventDefault();
 
+        // Almacenar una referencia al elemento td en el que se hizo clic derecho
+        tdSeleccionado = td;
+
         // Mostrar el menú personalizado en la posición del cursor
         menu.style.display = "block";
         menu.style.left = `${e.clientX}px`;
         menu.style.top = `${e.clientY}px`;
-
-        // Agregar un controlador de eventos click al botón del menú personalizado
-        menu.querySelector("button").addEventListener("click", function() {
-          // Cambiar el estilo del td correspondiente
-          if (td.style.background === "black") {
-            td.style.background = "white";
-            td.textContent = "";
-          } else {
-            td.style.background = "black";
-            td.textContent = null;
-          }
-
-          // Ocultar el menú personalizado después de hacer clic en el botón
-          menu.style.display = "none";
-        });
       });
+    });
+
+    // Agregar un controlador de eventos click al botón del menú personalizado
+    menu.querySelector("button").addEventListener("click", function() {
+      // Cambiar el estilo del td correspondiente
+      if (tdSeleccionado.style.background == "black") {
+        tdSeleccionado.style.background = "white";
+        tdSeleccionado.textContent = "-";
+      } else {
+        tdSeleccionado.style.background = "black";
+        tdSeleccionado.textContent = "-";
+      }
+      
+
+      // Ocultar el menú personalizado después de hacer clic en el botón
+      menu.style.display = "none";
     });
   }
 });
